217fc415903cf57d195869c9d1305b164738743a
cacheUp <bradb26@gmail.com>
2019-03-12 19:42:41 +0300

    database config

--- src/config/dev.js
+++ src/config/dev.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+
 export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl: 'mongodb://localhost:27017/use-my-tech'
+  dbUrl: `mongodb+srv://${process.env.USERNAME}:${
+    process.env.PASSWORD
+  }@cluster0-rrbed.mongodb.net/use-tech-stuff?retryWrites=true`
 }
